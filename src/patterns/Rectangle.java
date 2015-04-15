621ce18d962d033a3ed2a7edc8e47a0429b3a7ff
d154679 <d154679@gmail.com>
2015-04-15 14:42:54 +0800

    good man

---
+++ src/patterns/Rectangle.java
@@ -0,0 +1,22 @@
+package patterns;
+
+
+import patterns.Shape;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shane
+ */
+public class Rectangle implements Shape {
+
+   @Override
+   public void draw() {
+      System.out.println("Inside Rectangle::draw() method.");
+   }
+}
---
+++ src/patterns/Square.java
@@ -0,0 +1,19 @@
+package patterns;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shane
+ */
+public class Square implements Shape {
+
+   @Override
+   public void draw() {
+      System.out.println("Inside Square::draw() method.");
+   }
+}
